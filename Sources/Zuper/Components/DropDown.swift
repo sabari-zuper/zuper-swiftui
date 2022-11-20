bfe8693731de6504180322cf43e74501dad97c1a
Sabarinathan J <sabari@zuper.co>
2022-11-20 20:38:39 +0530

    Changed Select to DropDown
    - Now Select view changed to DropDown
    - For preview refer to 'Zuper Dropdown' Preview

--- Sources/Zuper/Components/DropDown.swift
+++ Sources/Zuper/Components/DropDown.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 /// Also known as dropdown. Offers a simple form control to select from many options.
 /// - Important: Component expands horizontally unless prevented by `fixedSize` or `idealSize` modifier.
-public struct Select: View {
+public struct DropDown: View {
 
     @Binding private var messageHeight: CGFloat
     
@@ -63,7 +63,7 @@ public struct Select: View {
 }
 
 // MARK: - Inits
-public extension Select {
+public extension DropDown {
  
     /// Creates Zuper Select component.
     init(
@@ -92,7 +92,7 @@ public extension Select {
 }
 
 // MARK: - Previews
-struct SelectPreviews: PreviewProvider {
+struct DropDownPreviews: PreviewProvider {
 
     static var previews: some View {
         PreviewWrapper {
@@ -108,19 +108,19 @@ struct SelectPreviews: PreviewProvider {
     
     static var zuperDropDown: some View {
         VStack(spacing:20) {
-            Select("Dropdown with label", value: InputFieldPreviews.value)
-            Select("",prefix: gridIcon, value: nil, placeholder: "Placeholder")
-            Select("Date picker dropdown", prefix: .none, value: nil, placeholder: "Placeholder", suffix: .datePicker)
+            DropDown("Dropdown with label", value: InputFieldPreviews.value)
+            DropDown("",prefix: gridIcon, value: nil, placeholder: "Placeholder")
+            DropDown("Date picker dropdown", prefix: .none, value: nil, placeholder: "Placeholder", suffix: .datePicker)
         }
         .previewDisplayName("Zuper Dropdown")
     }
 
     static var standalone: some View {
-        Select(InputFieldPreviews.label, prefix: gridIcon, value: InputFieldPreviews.value)
+        DropDown(InputFieldPreviews.label, prefix: gridIcon, value: InputFieldPreviews.value)
     }
 
     static var intrinsic: some View {
-        Select("Intrinsic", prefix: gridIcon, value: InputFieldPreviews.value)
+        DropDown("Intrinsic", prefix: gridIcon, value: InputFieldPreviews.value)
             .idealSize()
     }
 
@@ -137,45 +137,45 @@ struct SelectPreviews: PreviewProvider {
     }
 
     static func select(value: String, message: Message? = nil) -> some View {
-        Select(InputFieldPreviews.label, prefix: gridIcon, value: value, placeholder: InputFieldPreviews.placeholder, message: message)
+        DropDown(InputFieldPreviews.label, prefix: gridIcon, value: value, placeholder: InputFieldPreviews.placeholder, message: message)
     }
 
     @ViewBuilder static var storybookMix: some View {
         VStack(spacing: .medium) {
             Group {
-                Select("Label", value: "Value")
-                Select("", prefix: gridIcon, value: "Value")
-                Select("", prefix: .alertCircle, value: nil, placeholder: "Please select")
-                Select("Label (Empty Value)", prefix: .alertCircle, value: "")
-                Select("Label (No Value)", prefix: .alertCircle, value: nil, placeholder: "Please select")
-                Select("Label", prefix: .email, value: "Value")
+                DropDown("Label", value: "Value")
+                DropDown("", prefix: gridIcon, value: "Value")
+                DropDown("", prefix: .alertCircle, value: nil, placeholder: "Please select")
+                DropDown("Label (Empty Value)", prefix: .alertCircle, value: "")
+                DropDown("Label (No Value)", prefix: .alertCircle, value: nil, placeholder: "Please select")
+                DropDown("Label", prefix: .email, value: "Value")
             }
 
             Group {
-                Select("Label (Disabled)", prefix: .email, value: "Value", state: .disabled)
-                Select(
+                DropDown("Label (Disabled)", prefix: .email, value: "Value", state: .disabled)
+                DropDown(
                     "Label (Disabled)",
                     prefix: .email,
                     value: nil,
                     placeholder: "Please select",
                     state: .disabled
                 )
-                Select("Label (Modified)", prefix: .email, value: "Modified Value", state: .modified)
-                Select(
+                DropDown("Label (Modified)", prefix: .email, value: "Modified Value", state: .modified)
+                DropDown(
                     "Label (Modified)",
                     prefix: .email,
                     value: nil,
                     placeholder: "Please select",
                     state: .modified
                 )
-                Select(
+                DropDown(
                     "Label (Info)",
                     prefix: .alertCircle,
                     value: "Value",
                     message: .help("Help message, also very long and multi-line to test that it works.")
                 )
 
-                Select(
+                DropDown(
                     FieldLabelPreviews.longLabel,
                     labelAccentColor: .orangeNormal,
                     prefix: .email,
@@ -208,7 +208,7 @@ struct SelectLivePreviews: PreviewProvider {
                         Heading("Some content", style: .title3)
                         Text("Very long and multi-line follow-up text to test that it still all works correctly.")
 
-                        Select(
+                        DropDown(
                             "Label (Error) with a long multiline label to test that it works",
                             prefix: .email,
                             value: "Bad Value with a very long text that should overflow",
@@ -256,6 +256,6 @@ struct SelectDynamicTypePreviews: PreviewProvider {
     }
 
     @ViewBuilder static var content: some View {
-        SelectPreviews.standalone
+        DropDownPreviews.standalone
     }
 }
