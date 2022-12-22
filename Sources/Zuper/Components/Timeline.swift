bd8b870eacd8225c15d200b8983d82ffd7da2162
Sabarinathan J <sabari@zuper.co>
2022-12-22 21:20:09 +0530

    Added timeline view

---
+++ Sources/Zuper/Components/Timeline.swift
@@ -0,0 +1,256 @@
+import SwiftUI
+
+/// Shows progress on a process over time.
+///
+/// - Related components
+///   - ``TimelineItem``
+///
+/// - Note: [Orbit definition](https://orbit.kiwi/components/progress-indicators/timeline/)
+public struct Timeline<Content: View>: View {
+
+    @Environment(\.sizeCategory) var sizeCategory
+    @ViewBuilder let content: Content
+
+    public var body: some View {
+        if isEmpty == false {
+            VStack(alignment: .leading, spacing: Self.spacing) {
+                content
+            }
+            .backgroundPreferenceValue(TimelineItemPreferenceKey.self) { preferences in
+                GeometryReader { geometry in
+                    VStack(spacing: 0) {
+                        ForEach(preferences.indices.dropFirst(), id: \.self) { index in
+                            TimelineLine(
+                                height: progressLineHeight(for: index - 1, in: preferences, geometry: geometry),
+                                startPointType: preferences[index - 1].type,
+                                endPointType: preferences[index].type
+                            )
+                            .offset(x: TimelineIndicator.indicatorDiameter * sizeCategory.ratio / 2)
+                        }
+                    }
+                    .padding(.top, TimelineIndicator.indicatorDiameter * sizeCategory.ratio / 2)
+                }
+            }
+        }
+    }
+
+    var isEmpty: Bool {
+        content is EmptyView
+    }
+
+    func progressLineHeight(
+        for index: Int,
+        in preferences: TimelineItemPreferenceKey.Value,
+        geometry: GeometryProxy
+    ) -> CGFloat {
+        guard preferences.indices.contains(index) else { return 0 }
+
+        return geometry[preferences[index].bounds].height + Self.spacing
+    }
+
+    /// Creates Orbit TimelineItem component.
+    public init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+}
+
+// MARK: - Constants
+public extension Timeline {
+
+    static var spacing: CGFloat { .large }
+}
+
+// MARK: - Previews
+struct TimelinePreviews: PreviewProvider {
+
+    static var previews: some View {
+        PreviewWrapper {
+            standalone
+            mix
+        }
+        .padding(.medium)
+        .previewLayout(.sizeThatFits)
+    }
+
+    static var standalone: some View {
+        Timeline {
+            TimelineItem(
+                "Booked",
+                sublabel: "January 3, 10:43",
+                type: .past,
+                description: "You booked the trip and received e-tickets."
+            )
+            TimelineItem(
+                "Checked in",
+                sublabel: "May 3, 8:45",
+                type: .past,
+                description: "You checked in for the trip and received boarding passes"
+            )
+            TimelineItem(
+                "Board",
+                sublabel: "May 4, 8:15",
+                type: .past,
+                description: "Be at your departure gate at least 30 minutes before boarding."
+            )
+            TimelineItem(
+                "Board",
+                sublabel: "May 4, 8:15",
+                type: .past,
+                description: "Be at your departure gate at least 30 minutes before boarding."
+            )
+            TimelineItem(
+                "Board",
+                sublabel: "May 4, 8:15",
+                type: .present(),
+                description: "Be at your departure gate at least 30 minutes before boarding."
+            ) {
+                contentPlaceholder
+            }
+
+            TimelineItem(
+                "Arrive",
+                sublabel: "May 4, 11:49",
+                description: "Arrive at your destination"
+            )
+        }
+        .previewDisplayName("")
+    }
+
+    static var mix: some View {
+        VStack(alignment: .leading, spacing: .xxLarge) {
+            Timeline {
+                ForEach(steps) { step in
+                    TimelineItem(step.label, sublabel: step.sublabel, type: step.type, description: step.content)
+                }
+            }
+
+            Timeline {
+                ForEach(steps1) { step in
+                    TimelineItem(step.label, sublabel: step.sublabel, type: step.type, description: step.content)
+                }
+            }
+
+            Timeline {
+                ForEach(steps2) { step in
+                    TimelineItem(step.label, sublabel: step.sublabel, type: step.type, description: step.content)
+                }
+            }
+
+            Timeline {
+                ForEach(steps3) { step in
+                    TimelineItem(step.label, sublabel: step.sublabel, type: step.type, description: step.content)
+                }
+            }
+
+            Timeline {
+                ForEach(steps4) { step in
+                    TimelineItem(step.label, sublabel: step.sublabel, type: step.type, description: step.content)
+                }
+            }
+        }
+        .previewDisplayName("")
+    }
+
+    static var snapshot: some View {
+        standalone
+            .padding(.medium)
+    }
+
+    static let steps: [TimelineItemModel] = [
+        .init(0, texts[0].label, sublabel: texts[0].sublabel, type: .present(), content: texts[0].content),
+        .init(1, texts[1].label, sublabel: texts[1].sublabel, content: texts[1].content),
+        .init(2, texts[2].label, sublabel: texts[2].sublabel, content: texts[2].content),
+        .init(3, texts[3].label, sublabel: texts[3].sublabel, content: texts[3].content),
+        .init(4, texts[4].label, sublabel: texts[4].sublabel, content: texts[4].content),
+    ]
+
+    static let steps1: [TimelineItemModel] = [
+        .init(0, texts[0].label, sublabel: texts[0].sublabel, type: .past, content: texts[0].content),
+        .init(1, texts[1].label, sublabel: texts[1].sublabel, type: .past, content: texts[1].content),
+        .init(2, texts[2].label, sublabel: texts[2].sublabel, type: .past, content: texts[2].content),
+        .init(3, texts[3].label, sublabel: texts[3].sublabel, type: .present(), content: texts[3].content),
+        .init(4, texts[4].label, sublabel: texts[4].sublabel, content: texts[4].content),
+    ]
+
+    static let steps2: [TimelineItemModel] = [
+        .init(0, texts[0].label, sublabel: texts[0].sublabel, type: .past, content: texts[0].content),
+        .init(1, texts[1].label, sublabel: texts[1].sublabel, type: .past, content: texts[1].content),
+        .init(2, texts[2].label, sublabel: texts[2].sublabel, type: .past, content: texts[2].content),
+        .init(
+            3,
+            "Action required",
+            sublabel: texts[3].sublabel,
+            type: .present(.warning),
+            content: "The carrier has sent us a refund. There might be more depending on their policy."
+        ),
+        .init(4, texts[4].label, sublabel: texts[4].sublabel, content: texts[4].content),
+    ]
+
+    static let steps3: [TimelineItemModel] = [
+        .init(0, texts[0].label, sublabel: texts[0].sublabel, type: .past, content: texts[0].content),
+        .init(1, texts[1].label, sublabel: texts[1].sublabel, type: .past, content: texts[1].content),
+        .init(2, texts[2].label, sublabel: texts[2].sublabel, type: .past, content: texts[2].content),
+        .init(3, texts[3].label, sublabel: texts[3].sublabel, type: .past, content: texts[3].content),
+        .init(4, "Non refundable", sublabel: "25th Jun 10:48", type: .present(.critical), content: texts[4].content),
+    ]
+
+    static let steps4: [TimelineItemModel] = [
+        .init(1, texts[0].label, sublabel: texts[0].sublabel, type: .past, content: texts[0].content),
+        .init(2, texts[1].label, sublabel: texts[1].sublabel, type: .past, content: texts[1].content),
+        .init(3, texts[2].label, sublabel: texts[2].sublabel, type: .past, content: texts[2].content),
+        .init(4, texts[3].label, sublabel: texts[3].sublabel, type: .past, content: texts[3].content),
+        .init(5, texts[4].label, sublabel: "25th Jun 10:48", type: .present(.success), content: ""),
+    ]
+}
+
+extension TimelinePreviews {
+
+    struct TimelineItemModel: Identifiable {
+        let id: Int
+        let label: String
+        var sublabel = ""
+        var type: TimelineItemType = .future
+        let content: String
+
+        init(_ id: Int, _ label: String, sublabel: String = "", type: TimelineItemType = .future, content: String) {
+            self.id = id
+            self.label = label
+            self.sublabel = sublabel
+            self.type = type
+            self.content = content
+        }
+    }
+
+    struct TimelineItemTextModel {
+        let label: String
+        var sublabel = ""
+        let content: String
+    }
+
+    static let texts: [TimelineItemTextModel] = [
+        .init(
+            label: "Requested",
+            sublabel: "3rd May 14:04",
+            content: "We’ve received your request and will assign it to one of our agents."
+        ),
+        .init(
+            label: "In progress",
+            sublabel: "4th May 18:24",
+            content: "We’ll review your request and apply for any available refund from the carrier(s)."
+        ),
+        .init(
+            label: "Waiting for the carrier",
+            sublabel: "4th May 18:24",
+            content: "We’ve applied for a refund from the carrier(s)."
+        ),
+        .init(
+            label: "Carrier is refunding",
+            sublabel: "16th Jun 08:42",
+            content: "The carrier(s) is processing your refund request. We’ll contact them again if necessary."
+        ),
+        .init(
+            label: "Refunded",
+            content: "We’ll forward you all refunds from the carrier(s) after we receive it."
+        ),
+    ]
+}
---
+++ Sources/Zuper/Support/Components/TimelineItem.swift
@@ -0,0 +1,311 @@
+import SwiftUI
+
+/// One item of a Timeline.
+///
+/// - Related components
+///   - ``Timeline``
+///
+/// - Note: [Orbit definition](https://orbit.kiwi/components/progress-indicators/timeline/)
+public struct TimelineItem<Footer: View>: View {
+
+    @Environment(\.sizeCategory) var sizeCategory
+    @Environment(\.horizontalSizeClass) var horisontalSizeClass
+
+    let label: String
+    let sublabel: String
+    let description: String
+    let type: TimelineItemType
+    let badge: Badge
+    @ViewBuilder let footer: Footer
+
+    public var body: some View {
+        HStack(alignment: (hasHeaderContent || hasDescription) ? .firstTextBaseline : .top, spacing: .small) {
+
+            TimelineIndicator(type: type)
+            
+            VStack(alignment: .leading, spacing: .xSmall) {
+
+                headerWithAccessibilitySizeSupport
+                descriptionText
+                footer
+            }
+        }
+        .anchorPreference(key: TimelineItemPreferenceKey.self, value: .bounds) {
+            [TimelineItemPreference(bounds: $0, type: type)]
+        }
+    }
+
+    @ViewBuilder var headerWithAccessibilitySizeSupport: some View {
+        if horisontalSizeClass == .compact && sizeCategory.isAccessibilitySize {
+            VStack(alignment: .leading, spacing: .xSmall) {
+                header
+            }
+        } else {
+            HStack(spacing: .xSmall) {
+                header
+            }
+        }
+    }
+
+    @ViewBuilder var header: some View {
+        if label == "" {
+            badge
+        } else {
+            Heading(label, style: .title5, color: .custom(type.textColor))
+        }
+
+        Text(sublabel, size: .small, color: .custom(type.textColor))
+            .padding(.leading, sizeCategory.isAccessibilitySize ? .xSmall : 0)
+    }
+
+    @ViewBuilder var descriptionText: some View {
+        Text(description, size: .normal, color: .custom(type.textColor))
+    }
+
+    var hasHeaderContent: Bool {
+        label.isEmpty == false || sublabel.isEmpty == false
+    }
+
+    var hasDescription: Bool {
+        description.isEmpty == false
+    }
+}
+
+// MARK: - Inits
+
+public extension TimelineItem {
+
+    /// Creates Orbit TimelineItem component with text details and custom content at the bottom.
+    init(
+        _ label: String = "",
+        sublabel: String = "",
+        type: TimelineItemType = .future,
+        description: String = "",
+        badge: Badge = Badge(""),
+        @ViewBuilder footer: () -> Footer
+    ) {
+
+        self.label = label
+        self.sublabel = sublabel
+        self.type = type
+        self.description = description
+        self.badge = badge
+        self.footer = footer()
+    }
+}
+
+public extension TimelineItem where Footer == EmptyView {
+
+    /// Creates Orbit TimelineItem component with text details.
+    init(
+        _ label: String = "",
+        sublabel: String = "",
+        type: TimelineItemType = .future,
+        description: String = "",
+        badge: Badge = Badge("")
+    ) {
+        self.init(label, sublabel: sublabel, type: type, description: description,badge: badge, footer: { EmptyView() })
+    }
+}
+
+// MARK: - Types
+
+public enum TimelineItemType: Equatable {
+
+    public enum Status {
+        case success
+        case warning
+        case critical
+    }
+
+    case past
+    case present(Status? = nil)
+    case future
+
+    public var iconSymbol: Icon {
+        switch self {
+        case .past:                  return Icon(sfSymbol:"checkmark.circle.fill", color: Zuper.Status.success.color)
+        case .present(.critical):    return Icon(sfSymbol:"xmark.circle.fill", color: Zuper.Status.critical.color)
+        case .present(.warning):     return Icon(sfSymbol:"exclamationmark.circle.fill", color: Zuper.Status.warning.color)
+            //.alertCircle
+        case .present(.success):     return Icon(sfSymbol:"checkmark.circle.fill", color: Zuper.Status.success.color)
+            //.checkCircle
+        case .present:               return Icon(content: .none)
+            //.none
+        case .future:                return Icon(content: .none)
+            //.none
+        }
+    }
+
+    public var color: Color {
+        switch self {
+            case .past:                  return Zuper.Status.success.color
+            case .present(.critical):    return Zuper.Status.critical.color
+            case .present(.warning):     return Zuper.Status.warning.color
+            case .present(.success):     return Zuper.Status.success.color
+            case .present:               return Zuper.Status.success.color
+            case .future:                return .cloudNormalHover
+        }
+    }
+
+    public var isCurrentStep: Bool {
+        switch self {
+            case .present:
+                return true
+            case .past, .future:
+                return false
+        }
+    }
+
+    public var textColor: UIColor {
+        isCurrentStep ? .inkDark : .inkLight
+    }
+
+    public var isLineDashed: Bool {
+        switch self {
+            case .future:
+                return true
+            case .past, .present:
+                return false
+        }
+    }
+}
+
+struct TimelineItemPreferenceKey: SwiftUI.PreferenceKey {
+
+    typealias Value = [TimelineItemPreference]
+
+    static var defaultValue: Value = []
+
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
+struct TimelineItemPreference {
+
+    let bounds: Anchor<CGRect>
+    let type: TimelineItemType
+}
+
+// MARK: - Previews
+struct TimelineItemPreviews: PreviewProvider {
+
+    static var previews: some View {
+        PreviewWrapper {
+            standalone
+            snapshots
+        }
+        .previewLayout(.sizeThatFits)
+    }
+
+    static var standalone: some View {
+        TimelineItem(
+            sublabel: "3rd May 14:04",
+            type: .past,
+            description: "We’ve assigned your request to one of our agents.",
+            badge: Badge("Status")
+        )
+        .padding()
+        .previewDisplayName("Timeline")
+    }
+
+    static var snapshots: some View {
+        VStack(alignment: .leading, spacing: .xLarge) {
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .future,
+                description: "We’ve assigned your request to one of our agents."
+            )
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .present(),
+                description: "We’ve assigned your request to one of our agents."
+            )
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .present(.warning),
+                description: "We’ve assigned your request to one of our agents."
+            )
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .present(.critical),
+                description: "We’ve assigned your request to one of our agents."
+            )
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .past,
+                description: "We’ve assigned your request to one of our agents."
+            )
+            TimelineItem(
+                type: .present(.warning)
+            ) {
+                contentPlaceholder
+            }
+        }
+        .padding()
+        .previewDisplayName("Timeline")
+    }
+}
+
+struct TimelineItemCustomContentPreviews: PreviewProvider {
+
+    static var previews: some View {
+        PreviewWrapper {
+            customContent
+        }
+        .previewLayout(.sizeThatFits)
+    }
+
+    @ViewBuilder static var customContent: some View {
+        VStack(alignment: .leading) {
+            TimelineItem(
+                "Requested",
+                sublabel: "3rd May 14:04",
+                type: .future,
+                description: "We’ve assigned your request to one of our agents."
+            ) {
+                Button("Add info")
+            }
+
+            TimelineItem(
+                type: .present(.warning),
+                description: "We’ve assigned your request to one of our agents."
+            ) {
+
+                VStack {
+                    Text(
+                        "1 Passenger must check in with the airline for a possible fee",
+                        size: .custom(50),
+                        color: .custom(TimelineItemType.present(.warning).textColor),
+                        weight: .bold
+                    )
+                    .padding(.leading, .xSmall)
+                }
+            }
+            TimelineItem(
+                type: .present(.warning),
+                description: "We’ve assigned your request to one of our agents."
+            ) {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 50, height: 50)
+            }
+
+            TimelineItem(
+                type: .present(.warning)
+            ) {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 50, height: 50)
+            }
+        }
+        .padding()
+        .previewDisplayName("Timeline")
+    }
+}
