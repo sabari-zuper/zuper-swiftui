59c319f4c3ef0a2c14321004dd21a3156c93dcb9
Sabarinathan J <sabari@zuper.co>
2024-01-22 11:43:25 +0530

    Added 0.5s delay to avoid multiple action

--- Sources/Zuper/Components/Button.swift
+++ Sources/Zuper/Components/Button.swift
@@ -471,7 +471,7 @@ struct SingleTapButtonModifier: ViewModifier {
         }).disabled(tapped)
   }
   private func buttonAction() {
-    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
       tapped = false
     }
   }
