01b096af66593265f7ddc279a0c1c56642121d8e
Sabarinathan Jayakodi <sabari@zuper.co>
2024-11-14 17:55:28 +0530

    Upgraded platform version to iOS 15

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let useBundledFonts = false
 
 let package = Package(
     name: "Zuper",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Zuper",targets: ["Zuper"]),
